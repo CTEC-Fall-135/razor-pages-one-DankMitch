884f8ad0fc38471ea6ce1a332684d062292291bd
Dank <156018995+DankMitch@users.noreply.github.com>
2024-11-26 14:25:19 -0800

    2nd Iteration
    Completed through data model step. At Scaffold Student pages

--- ContosoUniversity/ContosoUniversity/obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
+++ ContosoUniversity/ContosoUniversity/obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9276f1b96ae217acdc94c06e6fb922ac529bacf9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36794381b2b49067858f410c8caff760031904b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
